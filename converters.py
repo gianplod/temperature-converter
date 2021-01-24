694108facd916217fce952cf2e139f25ac4afaa5
gianplod <plodari.gianluca@hotmail.com>
2021-01-24 19:12:53 +0100

    commiting temperature_converter project (interface and functions)

---
+++ converters.py
@@ -0,0 +1,39 @@
+
+def temperature_converter(value, convert_from, convert_to):
+
+    def celsius_converter(value, convert_to):
+        """
+        Converts from Celsius to Fahrenheit or Kelvin.
+        """
+        if convert_to == "°F":
+            return f"{str((round(value * 9/5 + 32, 2)))} {convert_to}"
+        elif convert_to == "K":
+            return f"{str(round((value + 273.15), 2))} {convert_to}"
+
+
+    def fahrenheit_converter(value, convert_to):
+        """
+        Converts from Fahrenheit to Celsius or Kelvin.
+        """
+        if convert_to == "°C":
+            return f"{str(round((value - 32) * 5/9, 2))} {convert_to}"
+        elif convert_to == "K":
+            return f"{str(round((value - 32) * 5/9 + 273.15, 2))} {convert_to}"
+
+
+    def kelvin_converter(value, convert_to):
+        """
+        Converts from Kelvin to Celsius or Fahrenheit.
+        """
+        if convert_to == "°C":
+            return f"{str(round(value - 273.15, 2))} {convert_to}"
+        elif convert_to == "°F":
+            return f"{str(round((value - 273.15) * 9/5 + 32, 2))} {convert_to}"
+
+
+    converters = {"°C" : celsius_converter(value, convert_to),
+                  "°F" : fahrenheit_converter(value, convert_to), 
+                  "°K" : kelvin_converter(value, convert_to)
+                } 
+    return converters[convert_from]
+
---
+++ temp_converter.py
@@ -0,0 +1,69 @@
+import wx 
+import converters as conv
+
+class UnitConverterInterface(wx.Frame):
+    def __init__(self, parent, style, title):
+        super().__init__(parent, style=style, title=title, size=(310,200))
+
+        self.InitUI()
+        self.Centre()
+        self.Show()
+
+
+    def InitUI(self):
+
+        panel = wx.Panel(self)
+        font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
+        font.SetPointSize(9)
+        hbox = wx.BoxSizer(wx.VERTICAL)
+
+        fgs = wx.FlexGridSizer(rows=4, cols=2, vgap=10, hgap=15)
+
+        temp_value = wx.StaticText(panel, label="Temperature Value: ")
+        temp_value.SetFont(font)
+        from_temp = wx.StaticText(panel, label="From: ")
+        from_temp.SetFont(font)
+        to_temp = wx.StaticText(panel, label="To: ")
+        to_temp.SetFont(font)
+        calc_button = wx.Button(panel, label="Calculate")
+        calc_button.Bind(wx.EVT_BUTTON, self.on_press_calculate)
+        self.result = wx.StaticText(panel, label='', pos=(10,10))
+        font.SetPointSize(14)
+        self.result.SetFont(font)
+
+        self.tc1 = wx.TextCtrl(panel)
+        self.tc2_combo = wx.ComboBox(panel, choices=["°C", "°F", "K"], style=wx.CB_READONLY)
+        self.tc3_combo = wx.ComboBox(panel, choices=["°C", "°F", "K"], style=wx.CB_READONLY)
+
+        fgs.AddMany([(temp_value), (self.tc1, 1, wx.EXPAND), (from_temp), 
+        (self.tc2_combo, 1, wx.EXPAND), (to_temp, 1, wx.EXPAND), (self.tc3_combo, 1, wx.EXPAND), (calc_button),
+        (self.result, 1, wx.RIGHT )])
+
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+        
+        panel.SetSizer(hbox)
+    
+    def ShowMessage(self):
+        wx.MessageBox('You entered an invalid value. Try again.', 'Value Error',
+            style=wx.OK | wx.ICON_ERROR)
+
+    def on_press_calculate(self, event):
+        try:
+            self.result.Label = conv.temperature_converter(value=int(self.tc1.GetValue()),
+            convert_from=self.tc2_combo.GetValue(), 
+            convert_to=self.tc3_combo.GetValue())
+        except ValueError:
+            self.ShowMessage()
+
+
+def main():
+
+    app = wx.App()
+    frame = UnitConverterInterface(None, style=wx.MINIMIZE_BOX | 
+    wx.SYSTEM_MENU | wx.CAPTION |	 wx.CLOSE_BOX, title='Temperature Converter')
+    frame.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
