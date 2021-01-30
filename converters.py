9cf248b3766a8ee89afc56d7016c59897a6eaa24
gianplod <plodari.gianluca@hotmail.com>
2021-01-30 04:04:31 +0100

    fixing error in K unit

--- converters.py
+++ converters.py
@@ -10,7 +10,6 @@ def temperature_converter(value, convert_from, convert_to):
         elif convert_to == "K":
             return f"{str(round((value + 273.15), 2))} {convert_to}"
 
-
     def fahrenheit_converter(value, convert_to):
         """
         Converts from Fahrenheit to Celsius or Kelvin.
@@ -19,7 +18,6 @@ def temperature_converter(value, convert_from, convert_to):
             return f"{str(round((value - 32) * 5/9, 2))} {convert_to}"
         elif convert_to == "K":
             return f"{str(round((value - 32) * 5/9 + 273.15, 2))} {convert_to}"
-
 
     def kelvin_converter(value, convert_to):
         """
@@ -30,10 +28,8 @@ def temperature_converter(value, convert_from, convert_to):
         elif convert_to == "°F":
             return f"{str(round((value - 273.15) * 9/5 + 32, 2))} {convert_to}"
 
-
-    converters = {"°C" : celsius_converter(value, convert_to),
-                  "°F" : fahrenheit_converter(value, convert_to), 
-                  "°K" : kelvin_converter(value, convert_to)
-                } 
+    converters = {"°C": celsius_converter(value, convert_to),
+                  "°F": fahrenheit_converter(value, convert_to),
+                  "°K": kelvin_converter(value, convert_to)
+                  }
     return converters[convert_from]
-
